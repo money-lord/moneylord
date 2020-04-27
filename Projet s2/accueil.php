77c8f4c7b791268ae43e366a55d87138e41b04aa
francoisintech <63243316+francoisintech@users.noreply.github.com>
2020-04-27 11:07:10 +0200

    Pages accueil et menu (pas terminée)
    Création des différentes pages du site

---
+++ Projet s2/accueil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8" />
+          <link rel="stylesheet" type="text/css" href="css/style.css" />
+          <link rel="stylesheet" href="css/styleprojet.css" type="text/css" media="screen" />
+          <link rel="icon" type="image/png" href="Images/minilogo.png" />
+          <title>Connexion</title>
+      </head>
+      <body>
+        <section class="sectionindex">
+          <div class="imageindex">
+            <center>
+            <img src="Images/logo.png" class="logo" alt="Logo" />
+            </center>
+          </div>
+
+          <div>
+              <ul>
+                <li>CoinFlip</li>
+                <li>Roulette</li>
+                <li>Jeu des Couleurs</li>                
+              </ul>
+          </div>
+        </section>
+
+        <footer>
+          <center>
+            <p>© 2020-2020, moneylord.com, Inc. ou ses filiales</p>
+          </center>
+        </footer>
+      </body>
+  </html>
---
+++ Projet s2/menu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+  <html>
+      <head>
+          <meta charset="utf-8" />
+          <link rel="stylesheet" type="text/css" href="css/style.css" />
+          <link rel="stylesheet" href="css/styleprojet.css" type="text/css" media="screen" />
+          <link rel="icon" type="image/png" href="Images/minilogo.png" />
+          <title>Connexion</title>
+      </head>
+      <body>
+        <section class="sectionindex">
+          <div class="imageindex">
+            <center>
+            <img src="Images/logo.png" class="logo" alt="Logo" />
+            </center>
+          </div>
+
+          <div class="signup">
+            <center>
+            <h1 class="welcome">Inscription</h1>
+            <p>
+              <form class="formulaire">
+                <p class="field"><input type="text" name="login" placeholder="Nom d'utilisateur"><i class="icon-user icon-large"></i></p>
+            		<p class="field"><input type="text" name="n" placeholder="Nom"><i class="icon-user icon-large"></i></p>
+                <p class="field"><input type="text" name="prenom" placeholder="Prénom"><i class="icon-user icon-large"></i></p>
+            		<p class="field"><input type="password" name="password" placeholder="Mot de passe"><i class="icon-lock icon-large"></i></p><br>
+            		<button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+            	</form>
+              <a href="index.html">Vous avez un compte ?</a>
+            </p>
+            </center>
+          </div>
+        </section>
+
+        <footer>
+          <center>
+            <p>© 2020-2020, moneylord.com, Inc. ou ses filiales</p>
+          </center>
+        </footer>
+      </body>
+  </html>
