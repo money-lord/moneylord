b2d14acc64cedcead02f2fdd283b6cd92fde9f99
francoisintech <63243316+francoisintech@users.noreply.github.com>
2020-05-04 12:22:30 +0200

    Mon Git Fonctionne !

--- Projet s2/Function/function.php
+++ Projet s2/Function/function.php
@@ -181,7 +181,7 @@ function displayBalance($bdd){
 }
 
 function chat($bdd){
-/*
+
 	$data2 = $bdd->query('SELECT COUNT(ID) AS nbID FROM Chat '); // Début fonction pour supprimer les messages quand il y en a plus de 50 dans la bdd
 	$donnees = $data2->fetch();
 	$data2->closeCursor();
@@ -191,7 +191,7 @@ function chat($bdd){
 
 			$data2 = $bdd->prepare('DELETE FROM CHAT WHERE ID= ');
 		}
-	}*/
+	}
 	$data2 = $bdd->query('SELECT COUNT(ID) FROM Chat ');
 	echo '<div class="chat"><div class="messagesborder"><div class="messages"><div class="mask"></div>';
 	$data1 = $bdd->query('SELECT Pseudo,Message FROM Chat ORDER BY ID DESC LIMIT 10');
