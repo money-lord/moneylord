94bf754fbc25998d15e6174de2fa134c237b25e4
francoisintech <63243316+francoisintech@users.noreply.github.com>
2020-06-02 10:23:42 +0200

    commit pour pull

--- Projet s2/Function/functionAddBet.php
+++ Projet s2/Function/functionAddBet.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('Function/function.php');
+include('function.php');
 ?>
 
 <!DOCTYPE html>
@@ -11,31 +11,36 @@ include('Function/function.php');
       </head>
 
 			<body>
-				<form action="GameTwo.php" method="POST"><input type="number" min="0" value="" placeholder="Entrer votre Mise ici">
+				<div class="testBet">
+				<form action="GameTwo.php" method="POST">
+					<div class="bet">
+						<input type="number" min="0" value="" placeholder="Entrer votre Mise ici">
 						<button name="betRoulette" value="0" >Clear</button>
 						<button name="betRoulette" value="0.01">+0.01</button>
 						<button name="betRoulette" value="0.1">+0.1</button>
 						<button name="betRoulette" value="1">+1</button>
 						<button name="betRoulette" value="10">+10</button>
 						<button name="betRoulette" value="100">+100</button>
-						<button name="betRoulette" value="">ALL-IN</button></form>
+						<button name="betRoulette" value="">ALL-IN</button>
+					</div>
+				</form>
 
-						<div class="flexButtonRoulette">
-							<div class="redTeam">
-								<form action="GameTwo.php" method="POST">
-									<button class="chatBet">Miser Sur Le Rouge</button>
-								</form>
-							</div>
-							<div class="blackTeam">
-								<form action="GameTwo.php" method="POST">
-									<button class="chatBet">Miser Sur Le Noir</button>
-								</form>
-							</div>
-							<div class="mlTeam">
-								<form action="GameTwo.php" method="POST">
-									<button class="chatBet">Miser Sur MoneyLord</button>
-								</form>
-							</div>
+					<div class="flexButtonRoulette">
+						<div class="redTeam">
+							<form action="GameTwo.php" method="POST">
+								<button class="chatBet">Miser Sur Le Rouge</button>
+							</form>
 						</div>
-
+						<div class="blackTeam">
+							<form action="GameTwo.php" method="POST">
+								<button class="chatBet">Miser Sur Le Noir</button>
+							</form>
+						</div>
+						<div class="mlTeam">
+							<form action="GameTwo.php" method="POST">
+								<button class="chatBet">Miser Sur MoneyLord</button>
+							</form>
+						</div>
+					</div>
 				</body>
+			</body>
