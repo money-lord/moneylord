d9e624b98f1623eff9a4a77db5c89d8b77962d27
Vincent Corallo <corallo@intechinfo.fr>
2020-06-09 14:32:22 +0200

    Update home.php

--- Projet s2/home.php
+++ Projet s2/home.php
@@ -25,9 +25,11 @@ chat($bdd);
              <h1>Coinflip</h1>
            Viens te confronter avec un autre joueur</br> et tente de gagner le double de ta somme</br> misée au super jeu du coinflip !</br> 1 chance sur 2 de gagner, a toi de jouer !</br>
            </br><img class="ImgHomeCoin" src = "Images/coinflip.gif" >
+
            <form method="post" action="GameOne.php">
            </br><button type="submit">Jouer !</button></center>
            </form>
+
          </div>
 
 
@@ -35,10 +37,12 @@ chat($bdd);
            <center>
              <h1>Roulette</h1>
            Tu peux ici jouer au jeu le plus prisé des casinos, la roulette !</br> Tu peux multiplier ta mise jusqu'a 10</br> si tu tombe sur la case Money Lord, Bonne Chance !</br>
-           </br><img class="ImgHomeRoulette" src = "Images/roulette.gif" >
+           </br><img class="ImgHomeRoulette" src = "Images/roulette.gif" ></br></br></br>
+
            <form method="post" action="GameTwo.php">
            </br><button type="submit">Jouer !</button></center>
            </form>
+
         </div>
 
          <div class="homeGameColor">
@@ -46,9 +50,11 @@ chat($bdd);
              <h1>Couleur</h1>
            Choisis ici ta couleur favorite et tente de multiplier ta mise par 5 !</br> Le jeu le plus chanceux de notre site, viens remplir tes poches ici !</br>
            </br><img class="ImgHomeColor" src = "Images/couleur.gif" >
+
            <form method="post" action="GameTree.php">
            </br><button type="submit">Jouer !</button></center>
           </form>
+
         </div>
       </div>
         </div>
